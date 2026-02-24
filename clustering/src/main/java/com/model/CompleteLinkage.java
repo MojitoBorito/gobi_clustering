6d6995a2688393e26fff2cba380302ff745e2273
mitsopoulos <mitsopoulos@cip.ifi.lmu.de>
2026-02-24 18:23:28 +0100

    added basic linkage strategies

--- clustering/src/main/java/com/model/CompleteLinkage.java
+++ clustering/src/main/java/com/model/CompleteLinkage.java
@@ -1,13 +1,14 @@
 package com.model;
 
-public class SingleLinkage<T> implements ClusterDistance<T>{
+public class CompleteLinkage<T> implements ClusterDistance<T>{
+
     @Override
     public double distanceToCluster(T elem, Cluster<T> cluster) {
         if (cluster.isEmpty()) throw new RuntimeException("Can't calculate distance to empty cluster");
-        double minDistance = Double.POSITIVE_INFINITY;
+        double maxDistance = Double.POSITIVE_INFINITY;
         for (T item : cluster.getItems()) {
-            minDistance = Math.min(minDistance, cluster.getDistanceMetric().compute(elem, item));
+            maxDistance = Math.min(maxDistance, cluster.getDistanceMetric().compute(elem, item));
         }
-        return minDistance;
+        return maxDistance;
     }
 }
