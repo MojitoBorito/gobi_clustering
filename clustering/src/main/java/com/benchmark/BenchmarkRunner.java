83515de2c60537eca9526030430b6dd3cc7e2236
mitsopoulos <mitsopoulos@cip.ifi.lmu.de>
2026-02-25 19:53:24 +0100

    added some benchmarking stuff

---
+++ clustering/src/main/java/com/benchmark/BenchmarkRunner.java
@@ -0,0 +1,17 @@
+package com.benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws Exception {
+        Options opt = new OptionsBuilder()
+                .include(KmerBenchmark.class.getSimpleName())
+                .forks(1)
+                .warmupIterations(5)
+                .measurementIterations(10)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
