3d3a5b9ad11305ce7086305f909a1affd194e482
mitsopoulos <mitsopoulos@cip.ifi.lmu.de>
2026-02-26 11:49:05 +0100

    added intersectSize method

--- clustering/src/main/java/com/kmer/KSet.java
+++ clustering/src/main/java/com/kmer/KSet.java
@@ -2,7 +2,7 @@ package com.kmer;
 
 import java.util.Set;
 
-public class KSet<E> {
+public class KSet<E>{
     private final Set<E> kmers;
 
     public KSet(Set<E> kmers) {
@@ -11,5 +11,20 @@ public class KSet<E> {
 
     public Set<E> getKmers() {
         return kmers;
+    }
+
+    public static <E> int intersectSize(KSet<E> a, KSet<E> b) {
+        if (a.size() > b.size()) { // iterate smaller
+            KSet<E> tmp = a; a = b; b = tmp;
+        }
+        int count = 0;
+        for (E x : a.kmers) {
+            if (b.kmers.contains(x)) count++;
+        }
+        return count;
+    }
+
+    public int size() {
+        return kmers.size();
     }
 }
