3358bbd9133b2afc954e503b1aca5f1186d4f792
Mohit <mohitp042005@gmail.com>
2026-03-03 15:34:58 +0100

    changed Smartbuckets to take on values and not the elements

--- clustering/src/main/java/com/bucket/KmerLongSetBucket.java
+++ clustering/src/main/java/com/bucket/KmerLongSetBucket.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-public class KmerLongSetBucket <C extends Cluster<KmerLongSet>> implements SmartBuckets<Element<KmerLongSet>, C>{
+public class KmerLongSetBucket <C extends Cluster<KmerLongSet>> implements SmartBuckets<KmerLongSet, C>{
 
     int n;
     HashMap<Long, HashSet<C>> clusters;
@@ -19,9 +19,9 @@ public class KmerLongSetBucket <C extends Cluster<KmerLongSet>> implements Smart
     }
 
     @Override
-    public Set<C> getClusters(Element<KmerLongSet> key) {
+    public Set<C> getClusters(KmerLongSet key) {
         Set<C> resCluster = new HashSet<>();
-        long[] set = key.getValue().getSet();
+        long[] set = key.getSet();
         for (int i = 0; i < n; i++) {
             HashSet<C> clusterSet = clusters.getOrDefault(set[i], null);
             if (clusterSet == null) continue;
@@ -31,16 +31,16 @@ public class KmerLongSetBucket <C extends Cluster<KmerLongSet>> implements Smart
     }
 
     @Override
-    public void add(Element<KmerLongSet> key, C cluster) {
-        long[] set = key.getValue().getSet();
+    public void add(KmerLongSet key, C cluster) {
+        long[] set = key.getSet();
         for (int i = 0; i < n; i++) {
             clusters.computeIfAbsent(set[i], _ -> new HashSet<>()).add(cluster);
         }
     }
 
     @Override
-    public void removeCluster(Element<KmerLongSet> key, C cluster) {
-        long[] set = key.getValue().getSet();
+    public void removeCluster(KmerLongSet key, C cluster) {
+        long[] set = key.getSet();
         for (int i = 0; i < n; i++) {
             HashSet<C> clusterSet = clusters.getOrDefault(set[i], null);
             if (cluster == null) continue;
