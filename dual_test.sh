b1486870d3d9084a4ccaa299ef5afea25e6800ae
Mohit Patil <patil@cip.ifi.lmu.de>
2026-03-17 15:07:29 +0100

    test scrupt updates

--- dual_test.sh
+++ dual_test.sh
@@ -3,4 +3,4 @@ FW="/mnt/raidbio2/extdata/praktikum/genprakt/genprakt-ws25/Block/pig-data-rnaseq
 OUT="/mnt/biocluster/praktikum/genprakt/patil/Blockteil/dual_out2"
 JAR="/mnt/biocluster/praktikum/genprakt/patil/Blockteil/clustering/target/clustering-jar-with-dependencies.jar"
 
-java -Xmx100g -jar $JAR -umi $UMI -reads $FW -outDir "$OUT"
+java -Xmx100g -jar $JAR -umi $UMI -reads $FW -outDir "$OUT" -umi_length "12"
